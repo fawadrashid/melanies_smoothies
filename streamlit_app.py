35caa27f98d5dae6f3ed573e768b2ec72e4a517d
Fawad Rashid <fawad@ndataconsulting.com>
2024-04-24 16:21:37 +0500

    integration fruityvice API

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,9 +24,10 @@ if ingredients_list:
     
     ingredients_string = ''
 
-    for fruit_chose in ingredients_list:
+    for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chose + ' '
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+        st.subheader(fruit_chosen + ' Nutrition Information')
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
         fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
     
     st.write(ingredients_string)
